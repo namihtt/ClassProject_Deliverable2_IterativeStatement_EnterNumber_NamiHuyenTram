2478e6f02cecfa1e836090206d781d707ecc16d6
Nami <htram@mail.usf.edu>
2019-09-12 19:34:17 -0400

    Commit changes this 1st time to improve

--- Program.cs
+++ Program.cs
@@ -47,28 +47,43 @@ namespace ClassProject_Deliverable2_IterativeStatement_EnterNumber_NamiHuyenTram
                 // take user input and converse string type to integer type
                 input = int.Parse(Console.ReadLine());
 
-                // conditional statement to make sure user input fall in the range of 1 to 100.
-                if (input >= i && input <= 100) 
-                { 
-                    // for loop to repeat the message
-                    for (i = 1; input >= i && input <= 100; i++)
-                    {
-                        Console.WriteLine("You have entered " + input.ToString() +
-                            " .This is the current integer value in the loop: " + i);
-                    }
-                }
-                else
+                // call custom method DisplayInput
+                DisplayInput();
+
+                // this custom method make sure the number fall into the range of 1 to 100
+                void DisplayInput ()
                 {
-                    Console.WriteLine("Please only enter number from 1 to 100!");
+                    // conditional statement to make sure user input fall in the range of 1 to 100.
+                    if (input >= i && input <= 100)
+                    {
+                        // for loop to repeat the message
+                        for (i = 1; input >= i && input <= 100; i++)
+                        {
+                            Console.WriteLine("You have entered " + input.ToString() +
+                                " .This is the current integer value in the loop: " + i);
+                        }
+                    }
+
+                    // if user input fall out of the range, ask for another input
+                    else
+                    {
+                        Console.Write("Please only enter number from 1 to 100: ");
+
+                        // read the new input and converse data type
+                        input = int.Parse(Console.ReadLine());
+
+                        // call DisplayInput to recheck and display accordingly
+                        DisplayInput();
+                    }
                 }
             } // end of try
             catch
             {
-                Console.WriteLine("Your input was invalid. Please only enter number from 1 to 100!");
+                Console.WriteLine("Your input was invalid. Please run the program again and only enter number from 1 to 100!");
             } // end of catch
 
             // Instruct user to close the window
-            Console.WriteLine("Now press any key to exit and come back any time to play again.");
+            Console.WriteLine("Now press any key to exit.");
 
             // keep the program not stopping and wait for user input to close the window
             Console.ReadKey(true);
